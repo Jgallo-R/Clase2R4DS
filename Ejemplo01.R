7eb48674aa7b4d63cca77413b27c6dd4518bdc8b
Jgallo-R <jorgelgallos@gmail.com>
2020-06-05 15:56:21 -0500

    Empezamos la clase recondando los comandos basicos asi como las modificaciones al repositorio de github.

---
+++ Ejemplo01.R
@@ -0,0 +1,15 @@
+# Comentario
+# En Rstudio la combinacion de teclas CTRL + SHIFT + N apertura un nuevo editor
+
+# Modifico/Seteo/Configuro el directorio de trabajo 
+setwd("C:/Users/JGALLO/Desktop/Clase2R4DS")
+
+# Verificar : obtener el wd (working directory)
+getwd()
+
+# listar el contenido del wd.
+dir()
+
+help("dir")
+dir(pattern = "csv")
+dir(pattern = "R")
