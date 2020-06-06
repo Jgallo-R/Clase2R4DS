49c1c9f21d93472f8ff57fefc73ae1f3ac635d6d
Jgallo-R <jorgelgallos@gmail.com>
2020-06-06 01:37:22 -0500

    Archivos agregados al final de la clase

---
+++ Ejemplo08.R
@@ -0,0 +1,35 @@
+rm(list = ls())
+getwd()
+
+library(help  = "datasets")
+
+class(AirPassengers)
+help(ts)
+plot(AirPassengers)
+
+class(CO2)
+help(CO2)
+str(CO2)
+
+data(cars)
+data(CO2)
+
+library(car)
+data(Prestige)
+
+str(Prestige)
+head(Prestige)
+tail(Prestige)
+
+summary(Prestige)
+
+Prestige[is.na(Prestige$type), ]
+
+help("Prestige")
+# Analisis de correlacion entre todas las variables numericas
+cor(Prestige[,-6])
+
+library(corrplot)
+corrplot(cor(Prestige[,-6]))
+install.packages("corrplot")
+
---
+++ Ejemplo09.R
@@ -0,0 +1,3 @@
+  rm(list = ls())
+getwd()  
+dir()
